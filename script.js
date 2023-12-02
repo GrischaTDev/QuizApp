919b8aa557cd7b49946dfe97d7c7bf780c005c84
GrischaTDev <grischa.taenzer@gmail.com>
2023-12-02 23:41:05 +0100

    complete project

--- script.js
+++ script.js
@@ -42,6 +42,9 @@ let questions = [
 ];
 
 let currentQuestion = 0;
+let correctQuestions = 0;
+let AUDIO_SUCCESS = new Audio('sounds/success.mp3');
+let AUDIO_WRONG = new Audio('sounds/wrong.mp3');
 
 function init() {
     document.getElementById('questions-length').innerHTML = questions.length;
@@ -50,17 +53,69 @@ function init() {
 }
 
 function showQuestion() {
-    let question = questions[currentQuestion];
+    // show End Screen
+    if(currentQuestion >= questions.length) {
+        document.getElementById('finish-quiz').innerHTML = /* html */`
+        <div class="end-screen">
+            <h1>Herzlichen Glückwunsch <br>Sie haben das Quiz erfolgreich abgeschlossen!</h1>
+            <img src="./img/trophy-black.svg" alt="">
+            <b>Du hast ${correctQuestions} von ${questions.length} Fragen richtig beantwortet.</b>
+            <a href="./"><button onclick="restartGame()" class="btn btn-primary">Nochmal Spielen</button></a>
+        </div>
+        `;
+    } else { // show question
+        let percent = (currentQuestion +1) / questions.length;
+        percent = Math.round(percent * 100); // Erst wird der inhalt in der klammer ausgrechnet, dann gerundet und dann in die Variable gegeben.
+        document.getElementById('progress-bar').innerHTML = `${percent} %`;
+        document.getElementById('progress-bar').style = `width: ${percent}%`;
 
-    document.getElementById('questiontext').innerHTML = question['question'];
-    document.getElementById('answer_1').innerHTML = question['answer_1'];
-    document.getElementById('answer_2').innerHTML = question['answer_2'];
-    document.getElementById('answer_3').innerHTML = question['answer_3'];
-    document.getElementById('answer_4').innerHTML = question['answer_4'];
+        let question = questions[currentQuestion];
+        document.getElementById('current-question').innerHTML = currentQuestion+1;
+        document.getElementById('questiontext').innerHTML = question['question'];
+        document.getElementById('answer_1').innerHTML = question['answer_1'];
+        document.getElementById('answer_2').innerHTML = question['answer_2'];
+        document.getElementById('answer_3').innerHTML = question['answer_3'];
+        document.getElementById('answer_4').innerHTML = question['answer_4'];
+    }
 }
 
-function answer(selection) {
+function answer(selection) {  // Selection hat den Wert den wir über die onclick function im HTML übergeben haben.
     let question = questions[currentQuestion];
-    console.log('Selected answer ist', selection);
-    console.log('Current question is', question);
+    let selectedQuestionNumber = selection.slice(-1);  // Hier holen wir uns mit slice den letzten Buschstaben"In diesem fall ist es die 1" in die Variable!!!
+    let idOfRightAnswer = `answer_${question['right_answer']}`;
+
+    if(selectedQuestionNumber == question['right_answer']) {
+        document.getElementById(selection).parentNode.classList.add('bg-success');
+        AUDIO_SUCCESS.play();
+        correctQuestions++;
+    } else {
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+        AUDIO_WRONG.play();
+    }
+    document.getElementById('next-button').disabled = false; // Das disable auf dem Button wird zu enable getauscht
+}
+
+function nextQuestion() {
+    currentQuestion++;
+    document.getElementById('next-button').disabled = true;
+
+    resetAnswerButtons()
+    showQuestion();
+}
+
+function resetAnswerButtons() {
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+}
+
+function restartGame() {
+    currentQuestion = 0;
+    correctQuestions = 0;
 }
